9d6c4575df895e31b101ca559e3a5a7fc70ef265
mohammad0028 <69770059+mohammad0028@users.noreply.github.com>
2021-04-06 16:42:45 +0430

    Update splunk_event.py

--- django-splunk-logger/splunk_event.py
+++ django-splunk-logger/splunk_event.py
@@ -6,7 +6,7 @@ from django.conf import settings
 
 class SplunkEvent(object):
     def send_event(self, event, sourcetype="log"):
-        url = "https://{}:{}/services/collector/event" \
+        url = "http://{}:{}/services/collector/event" \
             .format(settings.SPLUNK_ADDRESS,
                     settings.SPLUNK_EVENT_COLLECTOR_PORT)
         headers = {'Authorization': "Splunk {}".format(settings.SPLUNK_TOKEN)}
